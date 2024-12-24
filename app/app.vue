9c13937f5e4a7a0ac4e9fdce3f0d5d9f13e5d7eb
MinThuKhant763 <85979978+MinThuKhant763@users.noreply.github.com>
2024-12-25 01:42:16 +0630

    Initial commit for Nuxt.js project

---
+++ app/app.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const { getImages } = useFile()
+
+useHead({
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Our Memorable Moments'
+const description = 'A collection of our most memorable moments'
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogSiteName: 'NuxtHub image gallery template',
+  ogImage: 'https://image-gallery.nuxt.dev/social-card.png',
+  twitterCard: 'summary_large_image'
+})
+
+await getImages()
+</script>
+
+<template>
+  <div
+    class="bg-black min-h-[100dvh] overflow-x-auto relative"
+    :class="{ 'flex flex-col md:block': $router.currentRoute.value.fullPath !== '/' }"
+  >
+    <UNotifications />
+    <NuxtPage />
+    <ImageThumbnailList :class="$router.currentRoute.value.fullPath !== '/' ? 'opacity-100 z-[9999]' : 'opacity-0 z-[-1]'" />
+  </div>
+</template>
